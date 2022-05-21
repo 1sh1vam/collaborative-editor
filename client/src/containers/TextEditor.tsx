3f66aa28eb9080a9c1a8540a4c69c543adfc6b9c
1sh1vam <shivam07.mt@gmail.com>
2022-05-21 13:30:40 +0530

    toolbar options

--- client/src/containers/TextEditor.tsx
+++ client/src/containers/TextEditor.tsx
@@ -2,6 +2,27 @@ import { useCallback } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
+var TOOLBAR_OPTIONS = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+
 const TextEditor = () => {
   const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
     if (!wrapper) return;
@@ -11,6 +32,9 @@ const TextEditor = () => {
 
     new Quill(element, {
       theme: 'snow',
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      }
     });
   }, []);
 
