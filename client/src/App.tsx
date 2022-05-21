1d89cd309237a3041db119447a08761061ffd237
1sh1vam <shivam07.mt@gmail.com>
2022-05-21 18:24:02 +0530

    generating document id and routing to the document

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import TextEditor from "./containers/TextEditor";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  return <TextEditor />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
