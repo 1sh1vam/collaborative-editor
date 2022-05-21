fb6557c20654821950760fc3b216084db87e96ee
1sh1vam <shivam07.mt@gmail.com>
2022-05-21 19:31:21 +0530

    saving the data in mongodb database

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
+import mongoose from 'mongoose';
 import { Server } from 'socket.io';
 import { Editor } from './models/editor';
+
+
+mongoose.connect('mongodb://localhost/google-docs');
 
 const io = new Server(3001, {
     cors: {
@@ -12,12 +16,16 @@ io.on('connection', (socket) => {
     console.log('connected');
     socket.on('get-document', async (documentId) => {
         const document = await findOrCreate(documentId);
-        const data = "";
         socket.join(documentId);
-        socket.emit('load-document', data);
+        socket.emit('load-document', document!.data);
 
-        socket.on('send-changes', (delta) => {
+        socket.on('send-changes', async (delta) => {
             socket.broadcast.to(documentId).emit('receive-changes', delta);
+        });
+
+        socket.on('save-document', async (data) => {
+            const editor = await Editor.findById(documentId);
+            await Editor.findByIdAndUpdate(documentId, { data });
         });
     });
 });
@@ -26,12 +34,13 @@ const findOrCreate = async (id: string) => {
     if (!id) return;
 
     const document = await Editor.findById(id);
+    if (document) return document;
 
-    const newDocument = await Editor.build({
+    const newDocument = Editor.build({
         _id: id,
         data: "",
     });
 
-    newDocument.save();
+    await newDocument.save();
     return newDocument;
 }
