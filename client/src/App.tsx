e047560253caed52f285ad7f2246a0199db3f4fa
1sh1vam <shivam07.mt@gmail.com>
2022-05-22 11:42:03 +0530

    adding the page to add docs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import TextEditor from "./containers/TextEditor";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import DocsContainer from "./containers/DocsContainer";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
-        <Route path="/documents/:id" element={<TextEditor />} />
+        {/* <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} /> */}
+        <Route path="/" element={<DocsContainer />} />
       </Routes>
     </Router>
   );
---
+++ client/src/containers/DocsContainer.tsx
@@ -0,0 +1,19 @@
+const DocsContainer = () => {
+    return (
+        <div className="docs-container">
+            <div className="docs-top">
+                <div className="docs-top-main">
+                    <p className="new-doc-title">Start a new document</p>
+                    <div className="new-doc">
+                        <img src="/assets/images/docs-blank-googlecolors.png" />
+                    </div>
+                </div>
+            </div>
+            <div className="docs-items-containers">
+                <div className=""></div>
+            </div>
+        </div>
+    )
+}
+
+export default DocsContainer;
