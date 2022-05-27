bbbfa3985e5e026e1764d82a39d36b224df62760
1sh1vam <shivam07.mt@gmail.com>
2022-05-27 19:32:01 +0530

    adding header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import TextEditor from "./containers/TextEditor";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import DocsContainer from "./containers/DocsContainer";
+import Header from "./containers/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/documents/:id" element={<TextEditor />} />
         <Route path="/" element={<DocsContainer />} />
