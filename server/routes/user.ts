8f4b0867fa69d7cffaee0d91f3898f9be61a5112
1sh1vam <shivam07.mt@gmail.com>
2022-05-28 12:56:36 +0530

    signin route

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { User } from "../models/users";
+import { Password } from "../services/password";
 
 
 const app = express.Router();
 
 app.post('/api/users/signup', [
     body('name')
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Name is required'),
     body('email')
         .isEmail()
         .withMessage('Email is not valid'),
@@ -39,3 +41,33 @@ async (req: Request, res: Response) => {
     res.status(201).send()
 });
 
+app.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email is not valid'),
+    body('password')
+        .trim('')
+        .notEmpty()
+        .withMessage('Password is required')
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+        return res.status(400).send({ message: 'Invalid credentials' });
+    }
+
+    const passwordMatches = await Password.compare(existingUser.password, password);
+
+    if (!passwordMatches) {
+        return res.status(400).send({ message: 'Invalid credentials' });
+    }
+
+    res.send(existingUser);
+})
