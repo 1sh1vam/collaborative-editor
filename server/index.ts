8df894fafda551e7409c382ab7a242083b1d44d4
1sh1vam <shivam07.mt@gmail.com>
2022-05-28 11:09:16 +0530

    user model

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,8 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
-}))
+}));
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
---
+++ server/models/users.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface UserDoc extends mongoose.Document {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+const User =  mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
