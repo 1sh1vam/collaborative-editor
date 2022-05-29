f672e0a6398022db3ce2456d08f60725d378686d
1sh1vam <shivam07.mt@gmail.com>
2022-05-29 13:12:43 +0530

    emmitting error

--- server/routes/web-socket.ts
+++ server/routes/web-socket.ts
@@ -4,34 +4,29 @@ import { Editor } from "../models/editor";
 const handleSocket =  async (socket: Socket) => {
     console.log('connected');
     socket.on('get-document', async (documentId: string) => {
-        const document = await findOrCreate(documentId);
+        const document = await find(documentId);
         socket.join(documentId);
-        socket.emit('load-document', document!.data);
+        if (!document) {
+            socket.emit('error', { message: 'Document does not exist' })
+        } else {
+            socket.emit('load-document', document!.data);
+        }
 
         socket.on('send-changes', async (delta: any) => {
             socket.broadcast.to(documentId).emit('receive-changes', delta);
         });
 
         socket.on('save-document', async (data: any) => {
-            const editor = await Editor.findById(documentId);
             await Editor.findByIdAndUpdate(documentId, { data });
         });
     });
 }
 
-const findOrCreate = async (id: string) => {
+const find = async (id: string) => {
     if (!id) return;
 
     const document = await Editor.findById(id);
     if (document) return document;
-
-    const newDocument = Editor.build({
-        _id: id,
-        data: "",
-    });
-
-    await newDocument.save();
-    return newDocument;
 }
 
 export { handleSocket };
