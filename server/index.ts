11ba30323343b725bd00efd298ed0d0b4adc9230
1sh1vam <shivam07.mt@gmail.com>
2022-05-29 11:19:35 +0530

    setting up the dev environment

--- server/index.ts
+++ server/index.ts
@@ -3,15 +3,22 @@ import { Server } from 'socket.io';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import cookieSession from 'cookie-session';
 import { handleSocket } from './routes/web-socket';
-
 
 mongoose.connect('mongodb://localhost/google-docs');
 
 const app = express();
+
+app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
+}));
+console.log('env', process.env.NODE_ENV)
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'dev',
 }));
 
 const server = http.createServer(app);
