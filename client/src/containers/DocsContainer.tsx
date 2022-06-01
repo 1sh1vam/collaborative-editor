fcc6577311a95552476dbf11551af7183fd1aea8
1sh1vam <shivam07.mt@gmail.com>
2022-06-01 18:42:58 +0530

    support for navigating to the doc

--- client/src/containers/DocsContainer.tsx
+++ client/src/containers/DocsContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DocList from "../components/DocList";
 import { useNavigate } from "react-router-dom";
 import useRequest from "../hooks/use-request";
@@ -7,10 +7,28 @@ interface DocCreateData {
     id: string;
 }
 
+interface Docs {
+    id: string;
+    data: any;
+    owner: string;
+    name: string;
+}
+
 const DocsContainer = () => {
     const names = ['First Doc', 'Hello there', 'Untitled']
+    const [docs, setDocs] = useState<Docs[]>([]);
     const navigate = useNavigate();
     const { status, sendRequest } = useRequest();
+    const mountedRef = useRef(false);
+
+    useEffect(() => {
+        if (!mountedRef.current) {
+            mountedRef.current = true;
+            sendRequest({ apiRoute: '/api/docs', method: 'get', body: {}, onSuccess: (data: Docs[]) => {
+                setDocs(data)
+            } });
+        }
+    }, [])
 
 
     const handleNewDoc = (e: React.SyntheticEvent) => {
@@ -20,7 +38,7 @@ const DocsContainer = () => {
     }
     console.log('status', status);
     return (
-        <div className="docs-container">
+        <div  className="docs-container">
             <div className="docs-top">
                 <div className="docs-top-main">
                     <p className="new-doc-title">Start a new document</p>
@@ -32,7 +50,7 @@ const DocsContainer = () => {
             <div className="docs-items-container">
                 <p className="doc-items-title">Documents</p>
                 <div className="doc-items">
-                    {names.map((name) => <DocList name={name} />)}
+                    {docs.map((doc) => <DocList onClick={() => navigate(`/documents/${doc.id}`)} name={doc.name} />)}
                 </div>
             </div>
         </div>
