b8ed554318ca7635a5b7b10a00587ffaa3d06c57
1sh1vam <shivam07.mt@gmail.com>
2022-06-01 20:53:58 +0530

    getting current user and navigating to form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,22 @@
 import TextEditor from "./containers/TextEditor";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import DocsContainer from "./containers/DocsContainer";
 import Header from "./containers/Header";
 import { useEffect, useRef, useState } from "react";
 import useRequest from "./hooks/use-request";
 import Signup from "./containers/Signup";
+import Skeleton from "react-loading-skeleton";
 
 interface UserDocs {
-  id: string;
   name: string;
   email: string;
-  password: string;
+  id: string;
 }
 
 function App() {
   const mountedRef = useRef(false);
-  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState<UserDocs>({ name: '', email: '', id: '' })
   const { status, sendRequest } = useRequest();
-  const [currentUser, setCurrentUser] = useState<UserDocs>({id: '', name: '', email: '', password: ''})
   useEffect(() => {
     if (!mountedRef.current) {
       mountedRef.current = true;
@@ -25,23 +24,25 @@ function App() {
         apiRoute: '/api/users/currentUser',
         method: 'get',
         body: {},
-        onSuccess: (data: UserDocs) => setCurrentUser(data),
-        onError: ({ status }) => {
-          if (status === 401) {
-            navigate('/signup')
-          }
+        onSuccess(data: UserDocs) {
+          setCurrentUser(data);
         }
        });
     }
   }, [])
+
+  console.log('status inside app', status);
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/documents/:id" element={<TextEditor />} />
-        <Route path="/" element={<DocsContainer />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+        {status.loading && <Skeleton width={500} height={100} />}
+        {!status.loading && (
+          <Routes>
+            <Route path="/" element={!currentUser.id ? <Navigate to="/signup" /> : <DocsContainer />} />
+            <Route path="/documents/:id" element={<TextEditor />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        )}
     </Router>
   );
 }
