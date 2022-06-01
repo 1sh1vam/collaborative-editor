adb25912c1da3f3b57c149f4c19d58cf96d66d14
1sh1vam <shivam07.mt@gmail.com>
2022-06-01 18:42:40 +0530

    props type for doc list

--- client/src/components/DocList.tsx
+++ client/src/components/DocList.tsx
@@ -1,6 +1,13 @@
-const DocList = ({ name }: { name: string }) => {
+import React from "react";
+
+interface Props {
+    name: string;
+    onClick(e: React.SyntheticEvent): void;
+}
+
+const DocList = ({ name, ...props }: Props) => {
     return (
-        <div className="doc-list">
+        <div {...props} className="doc-list">
             <img src='/assets/images/docs_icon.png' />
             <p className="doc-name">{name}</p>
         </div>
