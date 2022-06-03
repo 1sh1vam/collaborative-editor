f79802ab99fa0b8b413456a84dacebeac4f340ee
1sh1vam <shivam07.mt@gmail.com>
2022-06-03 19:42:00 +0530

    change the name of doc

--- client/src/containers/Header.tsx
+++ client/src/containers/Header.tsx
@@ -1,21 +1,48 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { Docs } from "../App";
+import useRequest from "../hooks/use-request";
 
-const Header = () => {
+interface State {
+    docId?: string;
+}
+
+const Header = ({ docs } : { docs: Docs[] }) => {
     const location = useLocation();
+    let doc;
+
+    if (location.state) {
+        const { docId: id } = location.state as State;
+        doc = docs.find((data) => data.id === id);
+    }
 
     return (
         <div className="header">
             <div className="header-left">
                 <img src="/assets/images/docs_icon.png" />
-                {location.pathname === '/' ? <p className="header-title">Docs</p> : <FilenameInp />}
+                {location.pathname === '/' ? <p className="header-title">Docs</p> : <FilenameInp title={doc?.name} />}
             </div>
         </div>
     )
 }
 
-const FilenameInp = () => {
-    const [name, setName] = useState('');
+const FilenameInp = ({ title } : { title?: string }) => {
+    const [name, setName] = useState(title || '');
+    const { status, sendRequest } = useRequest();
+
+    useEffect(() => {
+        if (title) {
+            setName(title);
+        }
+    }, [title])
+
+    const saveName = () => {
+        sendRequest({
+            apiRoute: '/api/docs',
+            method: 'patch',
+            body: { name },
+        })
+    }
     return (
         <div className="file-input-container">
             <div className="hidden-file-name">{name}</div>
@@ -23,6 +50,7 @@ const FilenameInp = () => {
                 className="file-name-input"
                 onChange={({ target }) => setName(target.value)}
                 value={name}
+                onBlur={saveName}
             />
         </div>
     )
